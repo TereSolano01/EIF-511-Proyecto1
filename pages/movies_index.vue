205b00b8a74d057c07936457bb3db7ef0c1d88e9
TereSolano01 <tere.solano.solis@est.una.ac.cr>
2023-09-24 02:38:19 -0600

    style

--- pages/movies_index.vue
+++ pages/movies_index.vue
@@ -17,17 +17,12 @@
     <div class="container centered-content">
       <ul>
         <div class="grid-container">
-          <span v-for="movie in list" :key="movie._path" class="grid-item">
-              <NuxtLink :to="movie._path">
-                <NuxtLink :to="movie._path">{{ movie.title }}</NuxtLink>
-                <p class="goto"><NuxtLink :to="movie._path">Ver película<i class="fa-solid fa-chevron-right" style="color: #000000;"></i></NuxtLink></p>
-              </NuxtLink>
-            </span>
-          </div>
-     <li v-for="movie of movies" :key="movie.slug">
-      <img class="u-max-full-width" :src="'/images/'+movie.image" alt="Descripción de la imagen">
-       <NuxtLink :to="{ name: 'movies-slug', params: { slug: movie.slug } }">{{movie.Título}}</NuxtLink>
-     </li>
+          <li class="grid-item" v-for="movie of movies" :key="movie.slug">
+              <img class="u-max-full-width" :src="'/images/'+movie.image" alt="Descripción de la imagen">
+              <NuxtLink :to="{ name: 'movies-slug', params: { slug: movie.slug } }">{{movie.Título}}</NuxtLink>
+              <p class="goto"><NuxtLink :to="movie._path">Ver película<i class="fa-solid fa-chevron-right" style="color: #000000;"></i></NuxtLink></p>
+          </li>
+        </div>
    </ul>
   </div>
    <FooterView />
