b5e0e425a30a4bc7db6ebf705a4d3460cce34bcd
TereSolano01 <tere.solano.solis@est.una.ac.cr>
2023-09-24 02:19:42 -0600

    changes

--- pages/albumsNew_index.vue
+++ pages/albumsNew_index.vue
@@ -16,7 +16,12 @@
      <p>This section presents information about books</p>
    <ul>
      <li v-for="album of albums" :key="album.slug">
-       <NuxtLink :to="{ name: 'albums-slug', params: { slug: album.slug } }">{{album.title}}</NuxtLink>
+      <NuxtLink :to="album._path">
+    <img class="u-max-full-width" :src="'/images/'+album.image" alt="Descripción de la imagen">
+    <NuxtLink :to="album._path">{{ album.title }}</NuxtLink>
+    <p class="goto"><NuxtLink :to="album._path">Ver album<i class="fa-solid fa-chevron-right" style="color: #000000;"></i></NuxtLink></p>
+  </NuxtLink>
+    
      </li>
    </ul>
    <FooterView />
